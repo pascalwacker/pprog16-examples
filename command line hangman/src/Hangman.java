a80c61e84cd6a72b097762ffb4c7ac4b23cfafbd
Pascal Wacker <hello@pascalwacker.ch>
2016-02-29 21:56:11 +0100

    version 1 written in exercice class

--- command line hangman/src/Hangman.java
+++ command line hangman/src/Hangman.java
@@ -1,96 +1,178 @@
+package hangman;
+
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
 
-	/** This is the main method of our program. When the program starts, only
-	 * this method is invoked. It is static because it is not attached 
-	 * to an instance of the class Hangman. 
-	 */
 	public static void main(String[] args) {
-		// create a new instance of our game with the parameter "Programming"
-		Hangman game = new Hangman("Programming");
-		// run the method start() on our newly initialised game
-		game.start();
+		// sentence as input, take a random word from here
+		String[] words="Wilkommen zur Hangman Implementation in Java von Pascal Wacker".split(" ");
+		// get a random pointer for the words array
+		int idx = new Random().nextInt(words.length);
+		
+		// initialize new Game
+		Hangman game = new Hangman(words[idx]);
+		// run the game
+		game.play();
 	}
-	
 
-// ---- fields ----	
-	
-	/** The word to be guessed as an array of characters*/
-	private char[] word;
-
-	/** An array that indicates which character is visible (has been guessed)*/
-	private boolean visible[];
-
-	/** The amount of lives a player has */
-	private int lives;
-
-	/** Is true if all characters have been guessed and false otherwise */
-	private boolean solved;
-	
-	/** A helper object that reads the user input from the console */
+	// -- fields
+	char[] word;
+	int wordLength;
+	char[] guessed;
+	boolean[] used;
+	int turn;
+	int lives;
+	int charFound;
+	boolean finished;
 	private Scanner in;
 	
-	
-// ---- methods ----
-	
-	/**  Constructor - This is what gets executed when we create an object of this class. 
-	 * 		Here "this" is used because we have two variables called "word". 
-	 * 		"word" refers to the method argument and "this.word" refers
-	 * 		to the field of the object. 
-	 * 	In our constructor we generally want to initialise the object. 
-	 *  Often this means initialising the fields of the object. */
-	public Hangman(String word){
+	// -- constructor
+	public Hangman(String word) {
+		this.lives = 5;
+		this.turn = 0; // would be default
+		this.charFound = 0; // would be default
+		this.finished = false; // would be default
+		
+		// get the lenght of the input word
+		this.wordLength = word.length();
+		
+		// initialize word, guessed and used
 		this.word = word.toCharArray();
-		visible = new boolean[word.length()];
-		lives = 5;
-		solved = false;
-		in = new Scanner(System.in);
-	}
-	
-	public void start(){
-		System.out.println("Hi, welcome to Hangman");
-		System.out.println("Try to guess the word!");
-
-		// main game loop - we run this as long as the game is active
-		while(lives > 0 && !solved){
-			System.out.println("Guess a character!");
-			char input = in.next().charAt(0); // reads the user input
-			boolean correctGuess = false; // whether the user guessed correctly this turn
-
-			int numberOfGuessedChars = 0; // the number of already guessed characters
-			for(int i=0; i<word.length; i++){
-				if(word[i] == input){
-					visible[i] = true;
-					correctGuess = true;
-				}
-				if(visible[i]){
-					System.out.print(word[i]);
-					numberOfGuessedChars ++;
-				}
-				else{
-					System.out.print('_');
-				}
-				System.out.print(' ');
-			}
-			System.out.println(""); // print a new line
-
-			if(numberOfGuessedChars == word.length){
-				solved = true;
-			}
-			
-			if(!correctGuess){
-				lives --;
-			}
+		this.guessed = new char[this.wordLength];
+		this.used = new boolean[26];
+		Arrays.fill(this.used, false); // no character used yet
+		
+		// while
+		/*int i = 0;
+		while (i < this.wordLength) {
+			this.word[i] = Character.toUpperCase(this.word[i]);
+			this.guessed[i] = '-';
+			i++;
+		}
+		// unset i, if not needed anymore */
+		
+		// for every character
+		for (int i=0; i<this.wordLength; i++) {
+			// uppercase all characters
+			this.word[i] = Character.toUpperCase(this.word[i]);
+			// fill our helper array with `-`
+			this.guessed[i] = '-';
 		}
 		
-		if(solved){
-				System.out.println("Great, you won the game!");
-		}
-		else{
-				System.out.println("You have used up all your lives :(");
-		}
-
+		// open the Scanner
+		this.in = new Scanner(System.in);
 	}
+	
+	
+	
+	public void play() {
+		// print welcome
+		System.out.println("Wilkommen zu \"Hangman\", Implementation in Java von Pascal Wacker");
+		// print out the initial state
+		System.out.println(this.guessed);
+		
+		// while there are lives left and the game isn't won yet do ...
+		while (!this.finished && this.lives > 0) {
+			// tell user to input something
+			System.out.println((this.turn <= 0 ? "Bitte ersten Buchstaben eingeben:" : "Bitte nächsten Buchstaben eingeben:"));
+			
+			// read only one character as upper case
+			char input = Character.toUpperCase(this.in.next(".").charAt(0)); // there's an exception if it's not okay.
+			// int representation of the char (0 for A to 25 for Z)
+			int intOfInput = this.charToInt(input);
+			
+			// is it in the valid input range of A-Z?
+			if (intOfInput < 0 || intOfInput > 25) {
+				System.out.println("Es können nur Buchstaben von A bis Z verwendet werden, Gross/Kleinschreibung spielt keine Rolle!");
+			// if the char is used
+			} else if (this.used[intOfInput]) {
+				System.out.println("Der Buchstaben \"" + input + "\" wurde bereits benutzt.");
+			} else {
+				// set this char to used
+				this.used[intOfInput] = true;
+				
+				// check if the char is in our word and if yes at what position
+				int positionInWord = this.charInCharArray(input, this.word);
+				// if the char is there ...
+				if (positionInWord >= 0) {
+					// update the guessed array
+					this.updateGuessedFromInput(input);
+					// update the turn
+					this.turn++;
 
+					// check if we're done
+					// if (Arrays.equals(this.word, this.guessed)) {
+					if (this.charFound == this.wordLength) { // I guess, this should be better, regarding resources, since it hasn't to loop through 2 arrays!
+						this.finished = true;
+					}
+				} else {
+					// update turn
+					this.turn++;
+					// remove a live
+					this.lives--;
+					System.out.println("Leider kommt \"" + input + "\" nicht im Lösungswort vor." + (this.lives > 0 ? " Es verbleiben noch " + this.lives + " versuche!" : ""));
+				}
+			}
+			
+			// print out what we have so far
+			System.out.println(this.guessed);
+		}
+		// close the Scanner
+		this.in.close();
+		
+		// if there aren't any lives left, the user has lost
+		if (this.lives <= 0) {
+			System.out.println("Leider verloren. Das gesuchte Wort wäre \"" + String.copyValueOf(this.word) + "\" gewesen.");
+		} else {
+			System.out.println("Herzlichen Glückwunsch. Das gesuchte Wort \"" + String.copyValueOf(this.word) + "\" wurde gefunden in " + this.turn + " versuchen.");
+		}
+	}
+	
+	/**
+	 * Look for a character in a character array and return the index, default: -1
+	 * 
+	 * @param char needle
+	 * @param char[] charArray
+	 * @return int
+	 */
+	private int charInCharArray(char needle, char[] charArray) {
+		int position = -1;
+		int i = 0;
+		for (char c : charArray) {
+		    if (c == needle) {
+		    	position = i;
+		        break;
+		    }
+		    i++;
+		}
+		
+		return position;
+	}
+	
+	/**
+	 * Update the guessed array with a char input
+	 * 
+	 * @param char input
+	 */
+	private void updateGuessedFromInput(char input) {
+		for (int i=0; i<this.wordLength; i++) {
+			if (this.word[i] == input) {
+				this.guessed[i] = input;
+				this.charFound++;
+			}
+		}
+	}
+	
+	/**
+	 * Gives the integer of a char starting with 0 for A.
+	 * 
+	 * @param char input
+	 * @return int
+	 */
+	private int charToInt(char input) {
+		return ((int)input - 65);
+	}
 }
